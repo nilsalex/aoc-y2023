c0752028c56845a4a2da260a900e9b334d89a9b5
Nils Alex <nils@famalex.de>
2023-12-04 21:37:23 +0100

    day 4: use bytes instead of str

--- src/day04.rs
+++ src/day04.rs
@@ -1,32 +1,44 @@
 extern crate test;
 
-const INPUT: &str = include_str!("../inputs/day04.txt");
+const INPUT: &[u8] = include_bytes!("../inputs/day04.txt");
 
-fn get_winning_count(line: &str) -> u32 {
-    let numbers = line.split(": ").nth(1).unwrap();
-    let mut groups = numbers.split(" | ");
+const POWERS_OF_TEN: [u32; 2] = [1, 10];
+
+fn u32_from_bytes(bytes: &[u8]) -> u32 {
+    bytes.iter().rev().enumerate().fold(0, |acc, (ix, x)| {
+        acc + (x & 0x0f) as u32 * POWERS_OF_TEN[ix]
+    })
+}
+
+fn get_winning_count(line: &[u8]) -> u32 {
+    let numbers = line.split(|c| *c == b':').nth(1).unwrap();
+    let mut groups = numbers
+        .split(|c| *c == b'|')
+        .map(|group| group.trim_ascii_end());
     let winning: u128 = groups
         .next()
         .unwrap()
-        .split_whitespace()
+        .split(|c| *c == b' ')
+        .filter(|num| !num.is_empty())
         .fold(0, |acc, num| {
-            let num: u32 = num.parse().unwrap();
+            let num: u32 = u32_from_bytes(num);
             acc | (1_u128 << (num - 1))
         });
     groups
         .next()
         .unwrap()
-        .split_whitespace()
+        .split(|c| *c == b' ')
+        .filter(|num| !num.is_empty())
         .filter(|num| {
-            let num: u32 = num.parse().unwrap();
+            let num: u32 = u32_from_bytes(num);
             winning & (1_u128 << (num - 1)) != 0
         })
         .count() as u32
 }
 
-fn part1(input: &str) -> usize {
+fn part1(input: &[u8]) -> usize {
     input
-        .lines()
+        .split(|c| *c == b'\n')
         .map(|line| {
             let winning = get_winning_count(line);
             if winning == 0 {
@@ -38,8 +50,11 @@ fn part1(input: &str) -> usize {
         .sum()
 }
 
-fn part2(input: &str) -> usize {
-    let winning_map: Vec<u32> = input.lines().map(get_winning_count).collect();
+fn part2(input: &[u8]) -> usize {
+    let winning_map: Vec<u32> = input
+        .split(|c| *c == b'\n')
+        .map(get_winning_count)
+        .collect();
 
     let mut result = 0;
 
@@ -58,7 +73,7 @@ fn part2(input: &str) -> usize {
 }
 
 pub fn main() {
-    let input = INPUT.trim_end();
+    let input = INPUT.trim_ascii_end();
 
     println!("{}", part1(input));
     println!("{}", part2(input));
@@ -69,29 +84,29 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    const TEST_INPUT: &str = include_str!("../test_inputs/day04.txt");
+    const TEST_INPUT: &[u8] = include_bytes!("../test_inputs/day04.txt");
 
     #[test]
     fn test_part1() {
-        let input = TEST_INPUT.trim_end();
+        let input = TEST_INPUT.trim_ascii_end();
         assert_eq!(part1(input), 13);
     }
 
     #[test]
     fn test_part2() {
-        let input = TEST_INPUT.trim_end();
+        let input = TEST_INPUT.trim_ascii_end();
         assert_eq!(part2(input), 30);
     }
 
     #[bench]
     fn bench_part1(b: &mut Bencher) {
-        let input = INPUT.trim_end();
+        let input = INPUT.trim_ascii_end();
         b.iter(|| part1(input))
     }
 
     #[bench]
     fn bench_part2(b: &mut Bencher) {
-        let input = INPUT.trim_end();
+        let input = INPUT.trim_ascii_end();
         b.iter(|| part2(input))
     }
 }
