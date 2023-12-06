118c08d3bc74f5150a4d034613e002da7940fbb0
Nils Alex <nils@famalex.de>
2023-12-06 18:38:01 +0100

    better performance for day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,62 +1,57 @@
 extern crate test;
 
-const INPUT: &str = include_str!("../inputs/day06.txt");
+const INPUT: &[u8] = include_bytes!("../inputs/day06.txt");
 
-fn part1(input: &str) -> usize {
-    let mut lines = input.lines();
-    let durations: Vec<f64> = lines
+fn collect_as_u32<'a>(it: impl Iterator<Item = &'a u8>) -> u32 {
+    it.fold(0, |acc, c| acc * 10 + (c - b'0') as u32)
+}
+
+fn collect_as_usize<'a>(it: impl Iterator<Item = &'a u8>) -> usize {
+    it.fold(0, |acc, c| acc * 10 + (c - b'0') as usize)
+}
+
+fn part1(input: &[u8]) -> usize {
+    let mut lines = input.split(|c| *c == b'\n');
+    let durations = lines
         .next()
         .unwrap()
-        .split_whitespace()
+        .split(|c| *c == b' ')
         .skip(1)
-        .map(|num| num.parse().unwrap())
-        .collect();
-    let times: Vec<f64> = lines
+        .filter(|group| !group.is_empty())
+        .map(|num| collect_as_u32(num.iter()));
+    let times = lines
         .next()
         .unwrap()
-        .split_whitespace()
+        .split(|c| *c == b' ')
         .skip(1)
-        .map(|num| num.parse().unwrap())
-        .collect();
+        .filter(|group| !group.is_empty())
+        .map(|num| collect_as_u32(num.iter()));
 
     std::iter::zip(durations, times)
         .map(|(t, s)| {
-            (
-                (t - (t * t - 4.0 * s).sqrt()) / 2.0,
-                (t + (t * t - 4.0 * s).sqrt()) / 2.0,
-            )
+            let d = ((t * t - 4 * s) as f64).sqrt();
+            let t = t as f64;
+            ((t + d - 2.0) / 2.0).ceil() as usize - ((t - d + 2.0) / 2.0).floor() as usize + 1
         })
-        .map(|(a, b)| (b - 1.0).ceil() as usize - (a + 1.0).floor() as usize + 1)
         .product()
 }
 
-fn part2(input: &str) -> usize {
-    let mut lines = input.lines();
-    let t: f64 = (lines
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .skip(1)
-        .collect::<String>())
-    .parse()
-    .unwrap();
-    let s: f64 = (lines
-        .next()
-        .unwrap()
-        .split_whitespace()
-        .skip(1)
-        .collect::<String>())
-    .parse()
-    .unwrap();
+fn part2(input: &[u8]) -> usize {
+    let mut lines = input.split(|c| *c == b'\n');
+    let t: usize = collect_as_usize(lines.next().unwrap().iter().filter(|c| c.is_ascii_digit()));
+    let s: usize = collect_as_usize(lines.next().unwrap().iter().filter(|c| c.is_ascii_digit()));
 
-    let a = (t - (t * t - 4.0 * s).sqrt()) / 2.0;
-    let b = (t + (t * t - 4.0 * s).sqrt()) / 2.0;
+    let d = ((t * t - 4 * s) as f64).sqrt();
+    let t = t as f64;
 
-    (b - 1.0).ceil() as usize - (a + 1.0).floor() as usize + 1
+    let a = (t - d + 2.0) / 2.0;
+    let b = (t + d - 2.0) / 2.0;
+
+    b.ceil() as usize - a.floor() as usize + 1
 }
 
 pub fn main() {
-    let input = INPUT.trim_end();
+    let input = INPUT.trim_ascii_end();
 
     println!("{}", part1(input));
     println!("{}", part2(input));
@@ -67,29 +62,29 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    const TEST_INPUT: &str = include_str!("../test_inputs/day06.txt");
+    const TEST_INPUT: &[u8] = include_bytes!("../test_inputs/day06.txt");
 
     #[test]
     fn test_part1() {
-        let input = TEST_INPUT.trim_end();
+        let input = TEST_INPUT.trim_ascii_end();
         assert_eq!(part1(input), 288);
     }
 
     #[test]
     fn test_part2() {
-        let input = TEST_INPUT.trim_end();
+        let input = TEST_INPUT.trim_ascii_end();
         assert_eq!(part2(input), 71503);
     }
 
     #[bench]
     fn bench_part1(b: &mut Bencher) {
-        let input = INPUT.trim_end();
+        let input = INPUT.trim_ascii_end();
         b.iter(|| part1(input))
     }
 
     #[bench]
     fn bench_part2(b: &mut Bencher) {
-        let input = INPUT.trim_end();
+        let input = INPUT.trim_ascii_end();
         b.iter(|| part2(input))
     }
 }
