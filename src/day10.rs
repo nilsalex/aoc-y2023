0a6c058804047a867e8ff7d21c5eddef43fb93e1
Nils Alex <nils@famalex.de>
2023-12-10 13:24:25 +0100

    day 10 part 2

--- src/day10.rs
+++ src/day10.rs
@@ -1,7 +1,5 @@
 extern crate test;
 
-use std::collections::HashSet;
-
 const INPUT: &[u8] = include_bytes!("../inputs/day10.txt");
 
 fn get_start_type(p: &(usize, usize), grid: &[Vec<u8>]) -> u8 {
@@ -9,8 +7,8 @@ fn get_start_type(p: &(usize, usize), grid: &[Vec<u8>]) -> u8 {
 
     let up = matches!(grid[y - 1][x], b'|' | b'7' | b'F');
     let down = matches!(grid[y + 1][x], b'|' | b'L' | b'J');
-    let left = matches!(grid[y][x-1], b'-' | b'L' | b'F');
-    let right = matches!(grid[y][x+1], b'-' | b'7' | b'J');
+    let left = matches!(grid[y][x - 1], b'-' | b'L' | b'F');
+    let right = matches!(grid[y][x + 1], b'-' | b'7' | b'J');
 
     match (up, down, left, right) {
         (true, true, false, false) => b'|',
@@ -66,7 +64,7 @@ fn part1(input: &[u8]) -> u32 {
 
     let mut length = 0;
     let mut p = start;
-    let mut prev = (start.0-1, start.1);
+    let mut prev = (start.0 - 1, start.1);
 
     loop {
         let adj = get_adjacent(&p, &grid);
@@ -88,7 +86,88 @@ fn part1(input: &[u8]) -> u32 {
 }
 
 fn part2(input: &[u8]) -> u32 {
-    0
+    let num_cols = input.iter().take_while(|c| **c != b'\n').count() + 2;
+    let dummy_row: Vec<u8> = vec![b'.'; num_cols];
+
+    let mut start = (0, 0);
+
+    let mut grid: Vec<Vec<u8>> = std::iter::once(&dummy_row[..])
+        .chain(input.split(|c| *c == b'\n'))
+        .chain(std::iter::once(&dummy_row[..]))
+        .enumerate()
+        .map(|(y, line)| {
+            let line = std::iter::once(&b'.')
+                .chain(line.iter())
+                .chain(std::iter::once(&b'.'))
+                .enumerate()
+                .map(|(x, c)| {
+                    if *c == b'S' {
+                        start = (x, y);
+                    }
+                    *c
+                })
+                .collect();
+            line
+        })
+        .collect();
+
+    grid[start.1][start.0] = get_start_type(&start, &grid);
+
+    let mut loop_grid: Vec<Vec<u8>> = vec![vec![b'.'; grid[0].len()]; grid.len()];
+
+    let mut p = start;
+    let mut prev = (start.0 - 1, start.1);
+
+    loop {
+        loop_grid[p.1][p.0] = grid[p.1][p.0];
+
+        let adj = get_adjacent(&p, &grid);
+
+        if adj.0 == prev {
+            prev = p;
+            p = adj.1;
+        } else {
+            prev = p;
+            p = adj.0;
+        }
+
+        if p == start {
+            break;
+        }
+    }
+
+    let mut result = 0;
+
+    for line in loop_grid {
+        let mut crossing_count: u8 = 0;
+        let mut entry = 0;
+        for c in line {
+            match c {
+                b'.' => {
+                    if crossing_count % 2 == 1 {
+                        result += 1;
+                    }
+                }
+                b'|' => {
+                    crossing_count += 1;
+                }
+                b'-' => {}
+                b'L' | b'F' => {
+                    entry = c;
+                }
+                b'7' | b'J' => match (entry, c) {
+                    (b'L', b'7') => crossing_count += 1,
+                    (b'F', b'J') => crossing_count += 1,
+                    (b'L', b'J') => {}
+                    (b'F', b'7') => {}
+                    _ => panic!(),
+                },
+                _ => panic!(),
+            }
+        }
+    }
+
+    result
 }
 
 pub fn main() {
