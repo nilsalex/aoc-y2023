71e110b1f45e27d7ab9787050fe1e1a88234a08b
Nils Alex <nils@famalex.de>
2024-02-18 15:35:39 +0100

    day 21 part 2

--- src/day21.rs
+++ src/day21.rs
@@ -4,25 +4,54 @@ use std::collections::HashSet;
 
 const INPUT: &[u8] = include_bytes!("../inputs/day21.txt");
 
-fn part1(input: &[u8], steps: usize) -> usize {
-    let mut start = (0, 0);
-    let mut rocks = HashSet::new();
+#[derive(Debug)]
+struct Grid {
+    rocks: HashSet<(isize, isize)>,
+    start: (isize, isize),
+    rows: usize,
+    cols: usize,
+}
 
-    for (row, line) in input.split(|&b| b == b'\n').enumerate() {
-        for (col, byte) in line.iter().enumerate() {
-            match byte {
-                b'S' => {
-                    start = (row, col);
+impl Grid {
+    fn parse(input: &[u8]) -> Self {
+        let mut rocks = HashSet::new();
+        let mut start = (0, 0);
+
+        for (row, line) in input.split(|&b| b == b'\n').enumerate() {
+            for (col, byte) in line.iter().enumerate() {
+                match byte {
+                    b'S' => {
+                        start = (row as isize, col as isize);
+                    }
+                    b'#' => {
+                        rocks.insert((row as isize, col as isize));
+                    }
+                    _ => {}
                 }
-                b'#' => {
-                    rocks.insert((row, col));
-                }
-                _ => {}
             }
+        }
+
+        let rows = input.split(|&b| b == b'\n').count();
+        let cols = input.iter().take_while(|&&b| b != b'\n').count();
+
+        Self {
+            rocks,
+            start,
+            rows,
+            cols,
         }
     }
 
-    let mut reachable = HashSet::from([start]);
+    fn is_rock(&self, pos: &(isize, isize)) -> bool {
+        self.rocks.contains(&(
+            pos.0.rem_euclid(self.rows as isize),
+            pos.1.rem_euclid(self.cols as isize),
+        ))
+    }
+}
+
+fn count_reachable(grid: &Grid, steps: usize) -> usize {
+    let mut reachable = HashSet::from([grid.start]);
 
     for _ in 0..steps {
         let mut next_reachable = HashSet::new();
@@ -34,7 +63,7 @@ fn part1(input: &[u8], steps: usize) -> usize {
                 (row, col - 1),
                 (row, col + 1),
             ] {
-                if !rocks.contains(&next) {
+                if !grid.is_rock(&next) {
                     next_reachable.insert(next);
                 }
             }
@@ -46,8 +75,33 @@ fn part1(input: &[u8], steps: usize) -> usize {
     reachable.len()
 }
 
+fn get_coefficients(f0: isize, f1: isize, f2: isize) -> (isize, isize, isize) {
+    let a = (f0 - 2 * f1 + f2) / 2;
+    let b = (-3 * f0 + 4 * f1 - f2) / 2;
+    let c = f0;
+
+    (a, b, c)
+}
+
+fn part1(input: &[u8], steps: usize) -> usize {
+    let grid = Grid::parse(input);
+    count_reachable(&grid, steps)
+}
+
 fn part2(input: &[u8]) -> usize {
-    0
+    let grid = Grid::parse(input);
+    let half_dist = (grid.rows - 1) / 2;
+
+    let f0 = count_reachable(&grid, half_dist);
+    let f1 = count_reachable(&grid, grid.rows + half_dist);
+    let f2 = count_reachable(&grid, 2 * grid.rows + half_dist);
+
+    let (a, b, c) = get_coefficients(f0 as isize, f1 as isize, f2 as isize);
+
+    let steps = 26501365;
+    let num_iterations = ((steps - half_dist) / grid.rows) as isize;
+
+    (a * num_iterations * num_iterations + b * num_iterations + c) as usize
 }
 
 pub fn main() {
@@ -67,13 +121,20 @@ mod tests {
     #[test]
     fn test_part1() {
         let input = TEST_INPUT.trim_ascii_end();
+        assert_eq!(part1(input, 1), 2);
+        assert_eq!(part1(input, 2), 4);
+        assert_eq!(part1(input, 3), 6);
+        assert_eq!(part1(input, 4), 9);
+        assert_eq!(part1(input, 5), 13);
         assert_eq!(part1(input, 6), 16);
     }
 
     #[test]
     fn test_part2() {
-        let input = TEST_INPUT.trim_ascii_end();
-        assert_eq!(part2(input), 0);
+        let f = |x: isize| (2 * x * x + 4 * x + 9);
+        let g = |x: isize| ((-7) * x * x + 22 * x - 1);
+        assert_eq!(get_coefficients(f(0), f(1), f(2)), (2, 4, 9));
+        assert_eq!(get_coefficients(g(0), g(1), g(2)), (-7, 22, -1));
     }
 
     #[bench]
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(test)]
 #![feature(byte_slice_trim_ascii)]
 
+use std::env;
+
 mod day01;
 mod day02;
 mod day03;
@@ -24,25 +26,54 @@ mod day20;
 mod day21;
 
 fn main() {
-    day01::main();
-    day02::main();
-    day03::main();
-    day04::main();
-    day05::main();
-    day06::main();
-    day07::main();
-    day08::main();
-    day09::main();
-    day10::main();
-    day11::main();
-    day12::main();
-    day13::main();
-    day14::main();
-    day15::main();
-    day16::main();
-    day17::main();
-    day18::main();
-    day19::main();
-    day20::main();
-    day21::main();
+    if let Some(day) = env::args().nth(1) {
+        match day.as_str() {
+            "01" => day01::main(),
+            "02" => day02::main(),
+            "03" => day03::main(),
+            "04" => day04::main(),
+            "05" => day05::main(),
+            "06" => day06::main(),
+            "07" => day07::main(),
+            "08" => day08::main(),
+            "09" => day09::main(),
+            "10" => day10::main(),
+            "11" => day11::main(),
+            "12" => day12::main(),
+            "13" => day13::main(),
+            "14" => day14::main(),
+            "15" => day15::main(),
+            "16" => day16::main(),
+            "17" => day17::main(),
+            "18" => day18::main(),
+            "19" => day19::main(),
+            "20" => day20::main(),
+            "21" => day21::main(),
+            _ => {
+                panic!("invalid argument for \"day\": {}", day)
+            }
+        }
+    } else {
+        day01::main();
+        day02::main();
+        day03::main();
+        day04::main();
+        day05::main();
+        day06::main();
+        day07::main();
+        day08::main();
+        day09::main();
+        day10::main();
+        day11::main();
+        day12::main();
+        day13::main();
+        day14::main();
+        day15::main();
+        day16::main();
+        day17::main();
+        day18::main();
+        day19::main();
+        day20::main();
+        day21::main();
+    }
 }
